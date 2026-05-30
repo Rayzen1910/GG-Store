ce84457dd3a031baf20baf4bacde9a107f2304a8
Rayzen1910 <rayzen1910@example.com>
2026-05-30 14:31:47 +0700

    Revert Account sidebar and main content position swap

---
+++ scratch_git_push15.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Revert Account sidebar and main content position swap"
+"C:\Program Files\Git\cmd\git.exe" push origin main
